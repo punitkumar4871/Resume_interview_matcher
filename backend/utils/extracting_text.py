f9c5fafd267e242d12956e17ae5f25b70fe171f8
Tejeswar001 <tejeswarnaidu9@gmail.com>
2025-07-13 16:23:40 +0530

    feat(frontend): initialize frontend project with Next.js, Tailwind CSS, and TypeScript
    - Added package.json with dependencies and scripts for Next.js development
    - Created postcss.config.mjs for Tailwind CSS integration
    - Added tsconfig.json for TypeScript configuration

---
+++ backend/utils/extracting_text.py
@@ -0,0 +1,26 @@
+import docx2txt
+import PyPDF2
+def extract_text_from_pdf(file_path):
+    text = ""
+    with open(file_path, 'rb') as file:
+        reader = PyPDF2.PdfReader(file)
+        for page in reader.pages:
+            text += page.extract_text()
+    return text
+
+def extract_text_from_docx(file_path):
+    return docx2txt.process(file_path)
+
+def extract_text_from_txt(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        return file.read()
+
+def extract_text(file_path):
+    if file_path.endswith('.pdf'):
+        return extract_text_from_pdf(file_path)
+    elif file_path.endswith('.docx'):
+        return extract_text_from_docx(file_path)
+    elif file_path.endswith('.txt'):
+        return extract_text_from_txt(file_path)
+    else:
+        return ""
